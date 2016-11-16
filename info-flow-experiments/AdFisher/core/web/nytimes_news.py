ec152509d88e30454e85be59fe0ec7c0902efc74
srujanapeddada <srujana.peddada@gmail.com>
2016-11-16 01:59:29 -0500

    NYT Search and Click first attempt

--- info-flow-experiments/AdFisher/core/web/nytimes_news.py
+++ info-flow-experiments/AdFisher/core/web/nytimes_news.py
@@ -30,6 +30,51 @@ class NYTNewsUnit (google_ads.GoogleAdsUnit):
     def __init__ (self, browser, log_file, unit_id, treatment_id, headless=False, proxy = None):
 	google_ads.GoogleAdsUnit.__init__(self,browser,log_file,unit_id, treatment_id, headless, proxy=proxy)
 
+    # Search for articles by category and keyword and click 
+    def read_NYT_articles (self, count=5, keyword=None, category=None, time_on_site=20):
+        self.driver.set_page_load_timeout (60)
+	self.driver.get('http://nytimes.com/')
+	valid_categories = ['world', 'national', 'politics', 'sports', 'opinion', 'technology',
+			    'science', 'business']
+
+	categoryTab = self.driver.find_element_by_css_selector('ul.mini-navigation-menu')
+	print ("Element found")
+
+
+	# check to make sure the category is valid
+	if (category.lower() in valid_categories):
+	    # Go to the page that serves the category
+	    categoryLink = categoryTab.find_element_by_partial_link_text(category.title())
+	    print ("Clicking on the link")
+	    print (categoryLink.get_attribute("innerHTML"))
+
+	    categoryLink.click()
+	    time.sleep (3)
+
+	    # find all links with the search keyword
+	    searchLinks = self.driver.find_elements_by_partial_link_text(keyword.title())	
+
+	    print ("links in unit: ", len(searchLinks))
+	    visited = 0
+		
+
+	    for link in searchLinks:
+		link.click()
+	        for handle in self.driver.window_handles:
+		    self.driver.switch_to.window(handle)
+		    print (self.driver.title())
+		    time.sleep(time_on_site)
+		    site = self.driver.current_url
+		    self.log('treatment', 'read_news', site)
+		    self.driver.close()
+		    self.driver.switch_to.window (self.driver.window_handles[0])
+		visited += 1
+		if (visited == count): break
+
+	    
+
+
+
     def get_recommendedStories (self):
         self.driver.set_page_load_timeout (60)
         self.driver.get ('http://www.nytimes.com/')
