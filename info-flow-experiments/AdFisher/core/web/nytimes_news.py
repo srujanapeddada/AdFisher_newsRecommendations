83005bdefc9f840e8548a65805710601a8e587ca
srujanapeddada <srujana.peddada@gmail.com>
2016-11-22 00:22:46 -0500

    Mostly working NYT search and click

--- info-flow-experiments/AdFisher/core/web/nytimes_news.py
+++ info-flow-experiments/AdFisher/core/web/nytimes_news.py
@@ -6,6 +6,8 @@ import google_ads
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 from HTMLParser import HTMLParser
 
@@ -34,55 +36,55 @@ class NYTNewsUnit (google_ads.GoogleAdsUnit):
     def read_NYT_articles (self, count=5, keyword=None, category=None, time_on_site=20):
         self.driver.set_page_load_timeout (60)
 	self.driver.get('http://nytimes.com/')
-	valid_categories = ['world', 'national', 'politics', 'sports', 'opinion', 'technology',
-			    'science', 'business']
+	valid_categories = ['World', 'U.S.', 'Politics', 'Business',
+			    'Opinion', 'Tech', 'Science', 'Health',
+			    'Sports', 'Arts', 'Style', 'Food', 'Travel']
 
 	categoryTab = self.driver.find_element_by_css_selector('ul.mini-navigation-menu')
 	print ("Element found")
 
 
 	# check to make sure the category is valid
-	if (category.lower() in valid_categories):
+	if (category in valid_categories):
 	    # Go to the page that serves the category
-	    categoryLink = categoryTab.find_element_by_partial_link_text(category.title())
+	    categoryLink = categoryTab.find_element_by_partial_link_text(category)
 	    print ("Clicking on the link")
 	    print (categoryLink.get_attribute("innerHTML"))
 
 	    categoryLink.click()
-	    time.sleep (3)
+	    time.sleep (5)
 
-	    # find all links with the search keyword
-	    searchLinks = self.driver.find_elements_by_partial_link_text(keyword.title())	
+	    visitedLinks = []
+	    index = 0
 
-	    print ("links in unit: ", len(searchLinks))
-	    visited = 0
-		
+	    # makes sure we visit at most count number of pages
+	    for visited in range (0, count):
+		self.driver.execute_script ("window.scrollTo(0, document.body.scrollHeight);")
+		time.sleep(3)
+		# searches for links on the page
+	        searchLinks = self.driver.find_elements_by_partial_link_text(keyword.title())	
+		print ("links in unit: ", len(searchLinks))
+		# if no links found then break out of the loop
+		if (len(searchLinks) == 0): 
+		    print ("No Links found on: ", keyword)
+		    break
+		# for each of the links found
 
-	    for link in searchLinks:
-		link.click()
-	        for handle in self.driver.window_handles:
-		    self.driver.switch_to.window(handle)
-		    print (self.driver.title())
+		if (index < len(searchLinks)):
+		    searchLinks[index].click()
+		    index += 1
 		    time.sleep(time_on_site)
 		    site = self.driver.current_url
+		    print ("Site: ", site)
 		    self.log('treatment', 'read_news', site)
-		    self.driver.close()
-		    self.driver.switch_to.window (self.driver.window_handles[0])
-		visited += 1
-		if (visited == count): break
-
-	    
-
-
+		    # Go back to the previous page
+		    self.driver.back()
 
     def get_recommendedStories (self):
         self.driver.set_page_load_timeout (60)
         self.driver.get ('http://www.nytimes.com/')
 	tim = str(datetime.now())
 
-
-	self.driver.execute_script ("window.scrollTo(0, document.body.scrollHeight);")
-	time.sleep(3)
 
 	recomms = self.driver.find_element_by_xpath ("//*[@id='recommendations']/div[5]")
 
